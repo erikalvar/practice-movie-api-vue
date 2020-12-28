c121387d6d5aec04bd3957cc0ea77bd740ff730c
erikalvar <erikalvarbass@gmail.com>
2020-12-28 16:30:43 -0500

    adds movies index to home view

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,34 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import MoviesSearch from "../views/MoviesSearch.vue";
+import MoviesShow from "../views/MoviesShow.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/search",
+    name: "movies-search",
+    component: MoviesSearch,
+  },
+  {
+    path: "/movies/:id",
+    name: "movies-show",
+    component: MoviesShow,
+  },
+  
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Movie Rater</h1>
+
+    <div v-for="movie in movies">
+      <img :src="movie.image">
+      <h1>{{ movie.title }}</h1>
+      <p>{{ movie.release_year }}</p>
+      <p>{{ movie.description }}</p>
+      <p>Thumbs up: {{ movie.thumbs_up }}</p>
+      <p>Thumbs down: {{ movie.thumbs_down }}</p>
+    </div>
   </div>
 </template>
 
+<style>
+img {
+  max-height: 250px;
+}
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      movies: {},
+    };
+  },
+  created: function () {
+    this.moviesIndex();
+  },
+  methods: {
+    moviesIndex: function () {
+      axios.get("api/movies").then((response) => {
+        this.movies = response.data;
+        console.log(this.movies);
+      });
+    },
+  },
+};
 </script>
