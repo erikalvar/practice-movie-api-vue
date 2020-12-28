6c09e14ebd40f76daa5df0b48e63ed86a299d97e
erikalvar <erikalvarbass@gmail.com>
2020-12-28 17:22:09 -0500

    adds searches index with title search and router link to show

--- src/views/MoviesSearch.vue
+++ src/views/MoviesSearch.vue
@@ -1,1 +1,56 @@
 
+<template>
+  <div class="home">
+    <h1>Movie Search</h1>
+
+    <input type="text" v-model="input">
+    <button type="button" v-on:click="moviesSearch()" >
+      Search
+    </button>
+
+    <p>{{ input }}</p>
+    <p>{{ searches }}</p>
+
+    <div v-for="movie in searches">
+      <router-link v-bind:to="`/movies/${movie.imdb_id}`">
+        <h1>{{ movie.title }}</h1>
+      </router-link>
+      <h1>{{ movie.title }}</h1>
+      <p>{{ movie.release_year }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+img {
+  max-height: 250px;
+}
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      searches: {},
+      input: "",
+    };
+  },
+  created: function () {
+    // this.moviesIndex();
+  },
+  methods: {
+    moviesSearch: function () {
+      axios
+        .get("api/searches/", {
+          params: { title: this.input },
+        })
+        .then((response) => {
+          this.searches = response.data;
+          // console.log(this.searches);
+        });
+    },
+  },
+};
+</script>
